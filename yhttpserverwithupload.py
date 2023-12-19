f62a80580519c9208f56567242e8e32d8bed6b72
yacaat <37926237+yacaat@users.noreply.github.com>
2023-12-19 18:20:27 +0300

    Update and rename yhttpupload.py to yhttpserverwithupload.py

--- yhttpserverwithupload.py
+++ yhttpserverwithupload.py
@@ -1,18 +1,15 @@
 import http.server
 import socketserver
 import os
-import re
-
+import cgi
 
 class FileUploadHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Check if the request is for the form
         if self.path == '/upload_form':
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             
-            # HTML form for file upload
             form_html = """
             <!DOCTYPE html>
             <html>
@@ -32,38 +29,37 @@ class FileUploadHandler(http.server.SimpleHTTPRequestHandler):
             
             self.wfile.write(form_html.encode('utf-8'))
         else:
-            # Serve files as usual
             super().do_GET()
 
     def do_POST(self):
-        # Parse the Content-Length header
-        content_length = int(self.headers['Content-Length'])
+        content_type, _ = cgi.parse_header(self.headers['Content-Type'])
         
-        # Read the POST data
-        post_data = self.rfile.read(content_length)
-        
-        # Get the filename from the Content-Disposition header
-        #filename = self.headers['Content-Disposition'].split('"')[1]
-        filename = re.search(r'filename="([^"]+)"', str(post_data)).group(1)
-
-        
-        # Combine the filename with the current working directory
-        filepath = os.path.join(os.getcwd(), filename)
-        
-        # Write the POST data to the file
-        with open(filepath, 'wb') as f:
-            f.write(post_data)
-        
-        # Send a response back to the client
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'File uploaded successfully.')
+        if content_type == 'multipart/form-data':
+            form_data = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD': 'POST'}
+            )
+            
+            file_item = form_data['file']
+            
+            filename = os.path.join(os.getcwd(), file_item.filename)
+            file_content = file_item.value
+            
+            with open(filename, 'wb') as f:
+                f.write(file_content)
+            
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'File uploaded successfully.')
+        else:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(b'Invalid content type')
 
 if __name__ == "__main__":
     port = 8000
-    
     Handler = FileUploadHandler
-    
     httpd = socketserver.TCPServer(("", port), Handler)
     
     print(f"Serving on port {port} from the current working directory")
